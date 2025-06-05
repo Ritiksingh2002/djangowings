e139554fd8eea4ec810e5189f7c6f8a8d996b4a3
Ritik <singhritik955@gmail.com>
2025-06-05 08:57:16 +0530

    created superuser and jwt

--- inventoryAPI/views.py
+++ inventoryAPI/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .models import Item
 from .serializers import ItemSerializer
@@ -9,6 +10,7 @@ from .serializers import ItemSerializer
 class ItemViewSet(viewsets.ModelViewSet ):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permissions_classes=[IsAuthenticated]
     def list(self,request):
         items=Item.objects.all()
         serializer=ItemSerializer(items,many=True)
--- myproject/settings.py
+++ myproject/settings.py
@@ -38,11 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'api',
     'bookmanagement',
     'inventoryAPI'
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
